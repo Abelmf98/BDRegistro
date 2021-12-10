8545fbc19809fec4a9c24358ce0d67584aeda9ea
Abelmf98 <91120898+Abelmf98@users.noreply.github.com>
2021-12-10 19:06:17 +0100

    Update BDRegistro.sql

--- BDRegistro.sql
+++ BDRegistro.sql
@@ -11,12 +11,24 @@ USE registro;
 -- TABLAS
 CREATE TABLE miniJuego(
   idMinijuego tinyint UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  Nombre varchar(20) NOT NULL
+  Nombre varchar(20) NOT NULL,
+  url varchar(100) NOT NULL
 );
 
 -- TABLAS
 CREATE TABLE Usuario(
-  idUsuario tinyint UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  idUsuario smallint UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   Nombre varchar(20) NOT NULL,
-  Correo varchar(50) NOT NULL
+  Apellidos varchar(30) NOT NULL,
+  nombreUsuario varchar(10) NOT NULL,
+  Correo varchar(50) NOT NULL UNIQUE,
+  Password varchar(20) NOT NULL
+);
+
+CREATE TABLE Preferencia(
+  idUsuario smallint UNSIGNED NOT NULL,
+  idMinijuego tinyint UNSIGNED NOT NULL,
+  CONSTRAINT FK_idUsuario FOREIGN KEY (idUsario) REFERENCES usuario(idUsario),
+  CONSTRAINT FK_idMinijuego FOREIGN KEY (idMinijuego) REFERENCES miniJuego(idMinijuego),
+  CONSTRAINT PK_preferencia PRIMARY KEY (idUsario, idMinijuego)
 );
