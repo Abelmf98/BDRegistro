d0546590e9c4e5ad4190aef2a4cfde6fae64213b
Abelmf98 <amansillafelipe.guadalupe@alumnado.fundacionloyola.net>
2021-12-17 14:05:46 +0100

    insert into

---
+++ sql/insertminijuegos.sql
@@ -0,0 +1,6 @@
+INSERT INTO miniJuego(Nombre, url)
+VALUES 	('Crazy Balls', '/minijuegos/crazyBalls'),
+	('Ligas', '/minijuegos/ligas'),
+	('Reciclaje', '/minijuegos/reciclaje'),
+	('Flashcards', '/minijuegos/flashcards'),
+	('Tabla Periodica', '/minijuegos/tablaPeriodica')
